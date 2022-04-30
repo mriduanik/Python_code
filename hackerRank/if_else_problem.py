288f3642265e8d037ddf2865d5f552f2e7568c19
sharif hossain anik <aoronno4@gmail.com>
2022-05-01 00:45:01 +0600

    change

---
+++ hackerRank/if_else_problem.py
@@ -0,0 +1,14 @@
+def method(n):
+    if n%2==1:
+        print("Weird")
+    elif n%2==0 and 2 < n < 5:
+        print("Not Werid")
+    elif n%2==0 and 6 < n < 20:  
+        print("Weird")
+    else:
+        print("Not Werid")
+        
+  
+n=int(input())
+method(n)
+           
